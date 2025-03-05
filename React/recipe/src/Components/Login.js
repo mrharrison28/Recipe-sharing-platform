b5cd435e4466ab2338e21d28f59f840dfcf08495
Harrison <mrharrison2809@gmail.com>
2025-03-05 13:35:46 +0530

    Update Login.js

--- React/recipe/src/Components/Login.js
+++ React/recipe/src/Components/Login.js
@@ -16,7 +16,7 @@ export default function Login() {
 
   const handle = (e) => {
     e.preventDefault();
-    axios.post('https://recipe-sharing-platform-back.onrender.com', log)
+    axios.post('http://localhost:2803/users/login', log)
       .then(res => {
         alert(res.data.message);
           navigate('/')
